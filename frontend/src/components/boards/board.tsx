7915690de42091dabdf773f74f71c1f2af5996ea
Kreoniz <kreoniz@mail.ru>
2025-04-10 16:49:43 +0600

    feat: style project card

--- frontend/src/components/boards/board.tsx
+++ frontend/src/components/boards/board.tsx
@@ -4,14 +4,15 @@ import { NavLink } from 'react-router';
 export function Board({ id, name, description, taskCount }: IBoard) {
   return (
     <NavLink
-      className="flex w-full items-end justify-between rounded-sm border-2 border-gray-300/50 p-3 transition hover:bg-gray-300/50"
+      className="flex w-full justify-between gap-1 rounded-sm border-2 border-gray-300/50 p-3 transition hover:bg-gray-300/50 max-sm:flex-col sm:items-end"
       to={`/board/${String(id)}`}
     >
       <div>
-        <p className="text-xl font-bold">{name}</p>
-        <p>{description}</p>
+        <p className="text-base font-bold sm:text-lg">{name}</p>
+        <p className="line-clamp-3 text-sm text-gray-600">{description}</p>
       </div>
-      <p>
+
+      <p className="text-xs text-gray-600">
         Кол-во тикетов: <span className="font-bold">{taskCount}</span>
       </p>
     </NavLink>
